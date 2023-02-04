61e23f6be90c01c908649d2ded24ff355210b84a
abhi19singh <40690770+abhi19singh@users.noreply.github.com>
2023-02-04 18:55:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require("express");
-const app = express();
+const app = require('express')();
 const port = process.env.PORT||5000;
 app.use(express.json());
-app.use(require("./routes/appRouter"));
+var http = require('http');
+
 // get driver connection
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+ 
+// app.listen(port, () => {
+//   // perform a database connection when server starts
+//   // dbo.connectToServer(function (err) {
+//   //   if (err) console.error(err);
+ 
+//   // });
+//   console.log(`Server is running on port: ${port}`);
+// });
+function addRoutes(){
+  var httpServer = http.createServer(app);
+  
+  
+  var applicationRouter = require('./routers/appRouter').router;
+app.use("/", applicationRouter);
+app.use(function (err, req, res, next) {
+
 });
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  // dbo.connectToServer(function (err) {
-  //   if (err) console.error(err);
- 
-  // });
+
+server = httpServer.listen(port, () =>{
   console.log(`Server is running on port: ${port}`);
 });
+}
+addRoutes();
+module.exports = server;
